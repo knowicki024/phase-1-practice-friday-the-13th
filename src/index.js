5b4817afcbefc2da6a90760c3e0a0b2ba900c070
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-29 12:41:49 -0500

    revised

--- src/index.js
+++ src/index.js
@@ -1,91 +1,78 @@
-//challenge 1 we need to create a fetch/.then 
-//with response to the json and returning movie images
 
-let movieData;
-let currentMovie;
+let movieData
+let currentMovie
 
+const movieList = document.querySelector("#movie-list")
+
+const movieDetailImage = document.querySelector("img#detail-image")
+const movieDetailTitle = document.querySelector("h1#title")
+const movieDetailYear = document.querySelector("h3#year-released")
+const movieDetailDescription = document.querySelector("p#description")
+
+const watchedButton = document.querySelector("button#watched")
+
+const bloodForm = document.querySelector("form#blood-form")
+
+
+function getMovies(){
 fetch("http://localhost:3000/movies")
 .then(response => response.json())
 .then(json => {
-    movieData = json;
+    movieData = json
 
     movieData.forEach(movie => {
-        createMovieImageInBar(movie);
+        addMovieToPage(movie)
     })
+    movieDetails(movieData[0])
 
-    showMovieDetail(movieData[0]);
+    toggleWatchedButton()
 
-    hookUpWatchedButton();
-
-    hookUpBloodForm()
+    addBloodAmountForm()
 })
+}
 
-function createMovieImageInBar(movie) {
-    let movieList = document.getElementById('movie-list');
+function addMovieToPage(movie) {
+    const movieImage = document.createElement("img")
+    movieImage.src = movie.image 
+    movieList.append(movieImage)
 
-    let movieImage = document.createElement("img");
-    movieImage.src = movie.image;
-    movieList.appendChild(movieImage)
-
-    movieImage.addEventListener('click', () => {
-        showMovieDetail(movie);
+    movieImage.addEventListener("click", () => {
+        movieDetails(movie)
     })
 }
 
-function showMovieDetail(movie) {
-    currentMovie = movie;
+function movieDetails(movie) {
 
-    let detailTitle = document.getElementById("title")
-    let detailImage = document.getElementById("detail-image")
-    let detailDescription = document.getElementById("description")
-    let deatilYearReleased = document.getElementById("year-released")
-    let watchedButton = document.getElementById("watched")
-    let bloodAmount = document.getElementById("amount")
-
-    detailTitle.textContent = movie.title;
-    detailImage.src = movie.image;
-    detailDescription.textContent = movie.description;
-    deatilYearReleased.textContent = movie.release_year;
-    watchedButton.textContent = movie.watched ? "Watched" : "Unwatched"
-    bloodAmount.textContent = movie.blood_amount;
+   currentMovie = movie
+    
+    movieDetailImage.src = currentMovie.image 
+    movieDetailTitle.textContent = currentMovie.title 
+    movieDetailYear.textContent = currentMovie.release_year
+    movieDetailDescription.textContent = currentMovie.description 
 }
 
-function hookUpWatchedButton() {
-    let watchedButton = document.getElementById("watched");
-    watchedButton.addEventListener('click', () => {
+function toggleWatchedButton() {
+        watchedButton.addEventListener("click", () => {
         currentMovie.watched = !currentMovie.watched;
-        watchedButton.textContent = currentMovie.watched ? "Watched" : "Unwatched";
+        watchedButton.textContent = currentMovie.watched? "Watched": "Unwatched"
     })
 }
 
-function hookUpBloodForm() {
-    const bloodForm = document.getElementById("blood-form");
-    bloodForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+function addBloodAmountForm() {
 
-        const amountToAdd = event.target["blood-amount"].value;
-        currentMovie.blood_amount += parseInt(amountToAdd);
+    bloodForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        console.log(event)
+        const amountToAdd = event.target['blood-amount'].value //grabbing event value
+        console.log(event.target['blood-amount'].value)
+        
+        currentMovie.blood_amount += parseInt(amountToAdd)
+        console.log(currentMovie.blood_amount)
 
-        document.getElementById("amount").textContent = currentMovie.blood_amount;
+        document.querySelector("span#amount").textContent = currentMovie.blood_amount;
 
-        event.target.removeEventListener();
+        event.target['blood-amount'].value = ""
+
     })
 }
-
-
-
-
-//challenge 2 create a function with a CLICK event listener
-//that shows the first movie in the ARRAY pulling
-//the data from html
-
-//challenge 3 is another click event where you call 
-//on the html for the movie data, so when you click
-//image it will give movie info 
-//**need to learn about keeping watch/unwatched */
-
-//challenge 4 also click event with the watched and unwatched
-
-//challenge 5 add blood to counter. call on #bloodform
-//add event listener and prevent default. raise it with parseINt
-//
+getMovies()
